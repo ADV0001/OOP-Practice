e5d1a197bad5e0c07223e475c24ebc577605b874
ADV0001 <ADV0001@auburn.edu>
2024-12-09 13:06:56 -0500

    Printed table to show books cleaned up

--- library.py
+++ library.py
@@ -33,6 +33,13 @@ class Library:
         self.address = address
         self.books = []
 
+        self.col_headers = {
+            'NUM':7 ,
+            'TITLE':25,
+            'AUTHOR':25,
+            'CATEGORY':25
+         }
+
     def add_book(self, book):
         self.books.append(book)
         print(book.name+" has been added to the library collection")
@@ -43,22 +50,47 @@ class Library:
     def search_by_author(self,author_name):
         pass
 
-    ##Should be able to create a visual table with all of the books
-    """
-    This should be the display used to show books
-    |--No.--|--Name--|---Author--|--Description--|
-    """
+    ##SHOULD BE ABLE TO SHOW STOCKING STATUS WITH TABLE (e.g IN STOCK, CHECKED OUT, AVAILABLE XYZ)
     def show_books(self):
         cnt = 1
+        self.print_spacers(cols=self.col_headers)
+        self.print_header()
+        self.print_spacers(cols=self.col_headers)
+
         for book in self.books:
-            print(str(cnt)+". Book: " + book.name +" Author: "+ book.author +" Category: "+book.category)
+            print("|"+str(cnt).center(self.col_headers['NUM'])+"|" + book.name.center(self.col_headers['TITLE'],) +"|"+ book.author.center(self.col_headers['AUTHOR']) +"|"+book.category.center(self.col_headers["CATEGORY"])+"|")
             cnt+=1
+        self.print_spacers(cols = self.col_headers)
+
     def checkout_book(self,book_name):
         pass
 
     def delete_book(self,book_name):
         pass
 
+    ##Formatting methods - Could potentially create own class for this (ACSII Table)
+    """IF you decide to go this route then header function or table class should be able
+       to take columns names and columns widths as input to create table
+   """
+    def print_header(self):
+        print("|{}|{}|{}|{}|".format("NO.".center(self.col_headers['NUM']),
+                                     "TITLE".center(self.col_headers['TITLE']),
+                                     "AUTHOR".center(self.col_headers["AUTHOR"]),
+                                     "CATEGORY".center(self.col_headers["CATEGORY"])))
+
+    def print_spacers(self,cols=None):
+        if cols == None:
+            col_sum = sum(self.col_headers.values()) + len(self.col_headers)-1
+            print("+{}+".format("-"*col_sum))
+        else:
+            spacer_str = "+"
+            for val in sorted(cols.values()):
+                spacer_str = spacer_str+("-"*val)+"+"
+            print(spacer_str)
+
+    def print_footer(self):
+        col_sum = sum(self.col_headers.values()) + len(self.col_headers)+1
+        print("+"+"-"*col_sum+"+")
 
 main_library = Library("Valentine Library","156 Technology Drive")
 book1 = Book("MANIFEST","Gene Jones","Self-Help")
@@ -68,5 +100,3 @@ main_library.add_book(book1)
 main_library.add_book(book2)
 
 main_library.show_books()
-
-
